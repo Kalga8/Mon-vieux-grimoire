85d919ae1315a79494b2f6d08a570178ed88e483
Justine Launay <jlaunay8@outlook.fr>
2024-09-04 18:52:08 +0200

    Routes auth

--- Back-end/routes/book.js
+++ Back-end/routes/book.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const bookCtrl = require('../controllers/book');
+const auth = require('../middleware/auth');
+
+router.post('/', auth, bookCtrl.createBook); // Créer livre //
+router.put('/:id', auth, bookCtrl.modifyBook); // Modifier livre //
+router.delete('/:id', auth, bookCtrl.deleteOneBook); // Supprimer un livre //
 
 module.exports = router;
