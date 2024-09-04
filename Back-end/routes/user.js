42e03c70444ebb9f5bfb2ae47d7bb609332d020e
Justine Launay <jlaunay8@outlook.fr>
2024-09-04 18:32:15 +0200

    Routes user

--- Back-end/routes/user.js
+++ Back-end/routes/user.js
@@ -2,4 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
 
+// Inscription //
+router.post('/signup', userCtrl.userSignup);
+
+// Login //
+router.post('/login', userCtrl.userLogin);
+
 module.exports = router;
