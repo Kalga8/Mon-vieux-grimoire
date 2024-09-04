446c337658e0219ff848b70a030aca6ac1e3a146
Justine Launay <jlaunay8@outlook.fr>
2024-09-04 18:27:09 +0200

    Inscription utilisateur

--- Back-end/controllers/user.js
+++ Back-end/controllers/user.js
@@ -1,1 +1,16 @@
 const Book = require('../models/user');
+const bcrypt = require('bcrypt');
+
+// Inscription //
+exports.userSignup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      user.save()
+        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+};
