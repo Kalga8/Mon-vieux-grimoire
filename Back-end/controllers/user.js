e13292b51697cdf490c85572bef2b73172abe3f0
Justine Launay <jlaunay8@outlook.fr>
2024-09-04 18:42:41 +0200

    Fonction login avec token

--- Back-end/controllers/user.js
+++ Back-end/controllers/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 // Inscription //
 exports.userSignup = (req, res, next) => {
@@ -14,3 +15,29 @@ exports.userSignup = (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
     })
 };
+
+// Login //
+exports.userLogin = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+    .then(user => {
+        if (!user) {
+            return res.status(401).json({ error: 'Paire login/mot de passe incorrecte!' });
+        }
+        bcrypt.compare(req.body.password, user.password)
+            .then(valid => {
+                if (!valid) {
+                    return res.status(401).json({ error: 'Paire login/mot de passe incorrecte!' });
+                }
+                res.status(200).json({
+                    userId: user._id,
+                    token: jwt.sign(
+                        { userId: user._id },
+                        process.env.TOKEN,
+                        { expiresIn: '24h' }
+                    )
+                });
+            })
+            .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+}
