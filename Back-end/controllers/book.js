eeb7f6ddf7610d84b07a2bdb0eba423ed16da610
Justine Launay <jlaunay8@outlook.fr>
2024-09-05 09:46:40 +0200

    Rechercher des livres

--- Back-end/controllers/book.js
+++ Back-end/controllers/book.js
@@ -68,3 +68,17 @@ exports.deleteBook = (req, res, next) => {
         })
         .catch(error => res.status(500).json({error}));
 };
+
+// Recherche d'un livre //
+exports.getOneBook = (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+        .then(book => res.status(200).json(book))
+        .catch(error => res.status(404).json({error}));
+};
+
+// Recherche de tous les livres //
+exports.getAllBooks = (req, res, next) => {
+    Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({error}));
+};
--- Back-end/routes/book.js
+++ Back-end/routes/book.js
@@ -7,5 +7,7 @@ const bookCtrl = require('../controllers/book');
 router.post('/', auth, multer, bookCtrl.createBook); // Créer livre //
 router.put('/:id', auth, multer, bookCtrl.modifyBook); // Modifier livre //
 router.delete('/:id', auth, bookCtrl.deleteBook); // Supprimer un livre //
+router.get('/:id', bookCtrl.getOneBook); // Rechercher un livre //
+router.get('/', bookCtrl.getAllBooks); // Rechercher tous les livres //
 
 module.exports = router;
