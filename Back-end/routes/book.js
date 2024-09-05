6bd46c27e9cb7f936b8e27c2a72066b6da997401
Justine Launay <jlaunay8@outlook.fr>
2024-09-05 18:00:03 +0200

    Livres mieux notés

--- Back-end/routes/book.js
+++ Back-end/routes/book.js
@@ -5,11 +5,12 @@ const multer = require('../middleware//multer-config');
 const bookCtrl = require('../controllers/book');
 
 router.post('/', auth, multer, bookCtrl.createBook); // Créer livre //
+router.get('/', bookCtrl.getAllBooks); // Rechercher tous les livres //
+router.get('/bestrating', bookCtrl.bestrating); // Obtenir les meilleurs livres //
 router.put('/:id', auth, multer, bookCtrl.modifyBook); // Modifier livre //
 router.delete('/:id', auth, bookCtrl.deleteBook); // Supprimer un livre //
 router.post('/:id/rating', auth, bookCtrl.ratingBook); // Notation d'un livre //
 router.get('/:id', bookCtrl.getOneBook); // Rechercher un livre //
-router.get('/', bookCtrl.getAllBooks); // Rechercher tous les livres //
-router.get('/bestrating', bookCtrl.bestrating); // Obtenir les meilleurs livres //
+
 
 module.exports = router;
