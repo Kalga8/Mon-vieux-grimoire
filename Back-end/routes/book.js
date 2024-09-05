6d850a34e58ddf0e8157edd040fa28099ee9dc57
Justine Launay <jlaunay8@outlook.fr>
2024-09-05 09:27:01 +0200

    routes book multer

--- Back-end/routes/book.js
+++ Back-end/routes/book.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const bookCtrl = require('../controllers/book');
 const auth = require('../middleware/auth');
+const multer = require('../middleware//multer-config');
 
-router.post('/', auth, bookCtrl.createBook); // Créer livre //
-router.put('/:id', auth, bookCtrl.modifyBook); // Modifier livre //
-router.delete('/:id', auth, bookCtrl.deleteOneBook); // Supprimer un livre //
+router.post('/', auth, multer, bookCtrl.createBook); // Créer livre //
+router.put('/:id', auth, multer, bookCtrl.modifyBook); // Modifier livre //
+router.delete('/:id', auth, bookCtrl.deleteBook); // Supprimer un livre //
 
 module.exports = router;
