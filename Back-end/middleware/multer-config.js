46678b6e32d9507bb9d0e857eaefc3983b6a9386
Justine Launay <jlaunay8@outlook.fr>
2024-09-13 17:49:59 +0200

    Optimisation image test

--- Back-end/middleware/multer-config.js
+++ Back-end/middleware/multer-config.js
@@ -1,4 +1,7 @@
 const multer = require('multer');
+const sharp = require('sharp');
+path = require('path');
+fs = require('fs');
 
 const MIME_TYPES = {
     'image/jpg': 'jpg',
@@ -9,13 +12,39 @@ const MIME_TYPES = {
 
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
-      callback(null, 'images');
+    callback(null, 'images');
     },
+
     filename: (req, file, callback) => {
       const name = file.originalname.split(' ').join('_');
-      const extension = MIME_TYPES[file.mimetype];
-      callback(null, name + Date.now() + '.' + extension);
+      callback(null, name + Date.now() + '.webp');
     }
 });
+
+const upload = multer({ storage: storage}).single('image');
+
+const optimize = (req, res, next) => {
+  if (req.file) {
+      const filePath = req.file.path;
+      const output = path.join('images', `opt_${req.file.filename}`);
+      sharp(filePath)
+          .resize({ width: null, height: 568 })
+          .webp()
+          .toFile(output)
+          .then(() => {
+              //delete older file, keep the resized one
+              fs.unlink(filePath, () => {
+                  req.file.path = output;
+                  next();
+              })
+          })
+          .catch(err => next(err));
+  } else {
+      return next();
+  }
+};
+
+exports.upload = upload;
+exports.optimize = optimize;
 
 module.exports = multer({ storage: storage }).single('image');
--- Back-end/routes/book.js
+++ Back-end/routes/book.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-const multer = require('../middleware//multer-config');
+const multer = require('../middleware/multer-config');
 const bookCtrl = require('../controllers/book');
 
 router.post('/', auth, multer, bookCtrl.createBook); // Créer livre //
@@ -11,6 +11,5 @@ router.put('/:id', auth, multer, bookCtrl.modifyBook); // Modifier livre //
 router.delete('/:id', auth, bookCtrl.deleteBook); // Supprimer un livre //
 router.post('/:id/rating', auth, bookCtrl.ratingBook); // Notation d'un livre //
 router.get('/:id', bookCtrl.getOneBook); // Rechercher un livre //
-
 
 module.exports = router;
